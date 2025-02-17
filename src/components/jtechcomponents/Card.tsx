30f95f0ea83a29df36d194df059e188a14d5b29b
Bunjan, Mark <108747105+BunjanMark@users.noreply.github.com>
2025-02-17 18:56:05 +0800

    product page table made dynamic that can interact to database

--- src/components/jtechcomponents/Card.tsx
+++ src/components/jtechcomponents/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface CardProps {
   icon: React.ReactNode; // Or a more specific type if using an icon library
@@ -7,11 +7,17 @@ interface CardProps {
   percentageChange: string;
 }
 
-const Card: React.FC<CardProps> = ({ icon, title, value, percentageChange }) => {
+const jCard: React.FC<CardProps> = ({
+  icon,
+  title,
+  value,
+  percentageChange,
+}) => {
   return (
     <div className="card">
       <div className="card-content">
-        <div className="card-icon">{icon}</div> {/* Wrap icon in a div for styling */}
+        <div className="card-icon">{icon}</div>{" "}
+        {/* Wrap icon in a div for styling */}
         <div className="card-title">{title}</div>
         <div className="card-value">{value}</div>
         <div className="card-percentage">{percentageChange}</div>
@@ -20,4 +26,4 @@ const Card: React.FC<CardProps> = ({ icon, title, value, percentageChange }) => 
   );
 };
 
-export default Card;
+export default jCard;
